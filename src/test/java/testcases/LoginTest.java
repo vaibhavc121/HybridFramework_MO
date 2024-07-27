73605debd4307361a3f6504dcb96c099f0537f00
vaibhavc121 <vaibhavc121@gmail.com>
2024-07-27 09:08:31 +0530

    27-7

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,18 +1,22 @@
 package testcases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import base.BaseClass;
 
 public class LoginTest extends BaseClass
 {
+	public static final Logger logger = LogManager.getLogger(LoginTest.class);
+
 	@Test
 	void LoginApp()
 	{
-		Reporter.log("application opened");
-		Assert.assertTrue(false);
+		// Reporter.log("application opened");
+		logger.info("hii");
+		Assert.assertTrue(true);
 
 	}
 
